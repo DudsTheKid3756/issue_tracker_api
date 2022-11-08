fdd6bfe7979d64bdd36631bd18f9ec48d052d9d2
DudsTheKid3756 <curlyq3756@gmail.com>
2022-11-07 23:03:43 -0500

    initial commit

---
+++ src/main/java/duds_the_kid_3756/issue_tracker_api/IssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package duds_the_kid_3756.issue_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duds_the_kid_3756/issue_tracker_api/IssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package duds_the_kid_3756.issue_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
